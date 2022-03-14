b4932b0c0b225856b36045f8c497c0a6f3acabdb
Monde Nyawose <lionelnyawose@gmail.com>
2022-03-14 17:28:11 +0200

    fix premier page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
-(function(){
+(function(window){
+
+	// const videoEl = document.querySelector("#promo-video")
+	const menuToggle = document.querySelector(".mobile--menu");
+	const menu = document.getElementById("nav-icon3");
+	const body = document.querySelector("body");
 
 
-	console.log("in main")
-})()
+
+	menuToggle.addEventListener("click", toggleMenu)
+
+	function toggleMenu(e){
+		e.preventDefault();
+		body.classList.toggle("menu-open");
+		menu.classList.toggle("open");
+	}
+
+
+		window.onload = function(){
+			try{
+				ScrollOut({
+					cssProps: {
+						viewportY: true,
+						visibleY: true
+
+					}
+				});
+
+				console.log("loaded scroll")
+			}catch(err){
+				console.log(err + " this is the error")
+			}
+		}
+
+
+
+	
+})(window)
