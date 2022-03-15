8d5230ab1bea1eae57a186bf222f545e05d2bf1f
Monde Nyawose <lionelnyawose@gmail.com>
2022-03-15 19:24:06 +0200

    add content viewable code using sessionStorage API

--- js/view.js
+++ js/view.js
@@ -1,6 +1,20 @@
 (function(){
-
+	const videoItems = document.querySelectorAll(".viewable");
 	
+	videoItems.forEach(function(videoItem, i){
+		let vidId = videoItem.dataset.id;
+		videoItem.addEventListener("click", function(e){
+			e.preventDefault();
 
-	
+			// store the id in the sessionStorage
+			console.log(vidId)
+			videoUrl = `media/video/${vidId}.mp4`
+
+			sessionStorage.setItem("video-id", vidId);
+			sessionStorage.setItem("video-url", videoUrl);
+
+			window.location = "view.html"
+		})
+	})
+
 })()
