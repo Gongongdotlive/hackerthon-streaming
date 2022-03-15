58a260405e26bf64e38f5701567b02bba22cd375
Monde Nyawose <lionelnyawose@gmail.com>
2022-03-15 21:19:53 +0200

    update views

---
+++ js/premier.js
@@ -0,0 +1,20 @@
+(function(){
+		const videoItems = document.querySelectorAll(".viewable");
+	
+	videoItems.forEach(function(videoItem, i){
+		let vidId = videoItem.dataset.id;
+		videoItem.addEventListener("click", function(e){
+			e.preventDefault();
+
+			// store the id in the sessionStorage
+			console.log(vidId)
+			videoUrl = `media/video/${vidId}.mp4`
+
+			sessionStorage.setItem("video-id", vidId);
+			sessionStorage.setItem("video-url", videoUrl);
+			sessionStorage.setItem("premier", "true")
+
+			window.location = "view.html"
+		})
+	})
+})()
